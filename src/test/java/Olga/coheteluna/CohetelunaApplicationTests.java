6c73f5d5d63803f12ca710471cb79ae7d3fcd5cb
Olgasancha <olgasancha74@gmail.com>
2024-06-05 11:15:34 +0200

    Add files via upload

---
+++ src/test/java/Olga/coheteluna/CohetelunaApplicationTests.java
@@ -0,0 +1,13 @@
+package Olga.coheteluna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohetelunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
